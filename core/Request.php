8a7bf1a7f47a96984a88563535709169401fe9c9
mohamed agoumi <agoumi99@hotmail.com>
2021-03-16 21:21:56 +0100

    remove testing condition clean up empty lanes

--- core/Request.php
+++ core/Request.php
@@ -13,19 +13,18 @@
 
 class Request
 {
-	public function getPath()
+	public function getPath(): string
 	{
 		$path = $_SERVER['REQUEST_URI'] ?? '/';
 		$position = strpos($path, '?');
-		if ($position)
-		{
-			echo "hello";
-		}
+		if ($position === false)
+			return $path;
+		return substr($path, 0, $position);
 	}
 
 	public function getMethod()
 	{
-
+		return strtolower($_SERVER['REQUEST_METHOD']);
 	}
 
 	public function getBody()
