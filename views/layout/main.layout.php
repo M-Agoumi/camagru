f050a6bd01bc214b74f1d59d7e8351ad97efa7ad
mohamed agoumi <agoumi99@hotmail.com>
2021-03-16 21:27:05 +0100

    main layout of my views to be modified later

---
+++ views/layout/main.layout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{ title }}</title>
+	<style>
+		body {
+			background-color: indianred;
+		}
+	</style>
+</head>
+<body>
+	{{ body }}
+</body>
+</html>
