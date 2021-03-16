92d381e01ec83e64433f512512af0d79555f8af1
mohamed agoumi <agoumi99@hotmail.com>
2021-03-16 21:22:38 +0100

    response class for the response related methods now change http response code only

---
+++ core/Response.php
@@ -0,0 +1,20 @@
+<?php
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Response.php                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: magoumi <agoumi.mohamed@outlook.com>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/03/16 20:53:26 by magoumi           #+#    #+#              #
+#    Updated: 2021/03/16 20:53:26 by magoumi          ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+class Response
+{
+	public function setStatusCode(int $code)
+	{
+		http_response_code($code);
+	}
+}
