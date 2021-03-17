fc229f16a59c0517c97bcfef11bbf8cdfa4b7c77
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:07:14 +0100

    adding a css file

--- views/layout/main.layout.php
+++ views/layout/main.layout.php
@@ -4,14 +4,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>{{ title }}</title>
-	<style>
-		body {
-			background-color: indianred;
-		}
-	</style>
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-	{{ body }}
+<div class="wrapper">
+    <?php include "__header.php" ?>
+    <main class="page-body">
+        <div class="container">
+            {{ body }}
+        </div>
+    </main>
+</div>
+    <?php include "__footer.php" ?>
 </body>
 </html>
