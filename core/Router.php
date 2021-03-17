c861e0272d828eb37a33359f52dc4cfce8fa59ca
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:04:05 +0100

    small changes and documenting the class

--- core/Router.php
+++ core/Router.php
@@ -1,4 +1,16 @@
 <?php
+/* ************************************************************************************************ */
+/*                                                                                                  */
+/*                                                        :::   ::::::::   ::::::::  :::::::::::    */
+/*   Router.php                                        :+:+:  :+:    :+: :+:    :+: :+:     :+:     */
+/*                                                      +:+         +:+        +:+        +:+       */
+/*   By: magoumi <magoumi@student.1337.ma>             +#+      +#++:      +#++:        +#+         */
+/*                                                    +#+         +#+        +#+      +#+           */
+/*   Created: 2021/03/17 09:12:14 by magoumi         #+#  #+#    #+# #+#    #+#     #+#             */
+/*   Updated: 2021/03/17 09:12:14 by magoumi      ####### ########   ########      ###.ma           */
+/*                                                                                                  */
+/* ************************************************************************************************ */
+
 
 require_once "Request.php";
 require_once "Response.php";
@@ -9,19 +21,27 @@ class Router
 	public ?Response $response = null;
 	protected array $routes = [];
 
+	/**
+	 * Router constructor.
+	 * save an instance of request and response  from our application
+	 * so we can call them with $this whenever we need them
+	 * @param Request $request
+	 * @param Response $response
+	 */
 	public function __construct(Request $request, Response $response)
 	{
 		$this->request = $request;
 		$this->response = $response;
 	}
-	/*
-	 * @return type string the url of the current page
-	 */
-	public function getUrl(): string
-	{
-		return $_SERVER['PATH_INFO'] ?? '/';
-	}
 
+	/**
+	 * Store a callback in an array with its associated
+	 * path (get method)
+	 *
+	 * @param $path
+	 * @param $callback
+	 * does not return anything
+	 */
 	public function get($path, $callback)
 	{
 		if (!isset($this->routes['get'][$path]))
@@ -30,6 +50,15 @@ class Router
 			die("route $path is already used please update it");
 	}
 
+	/**
+	 * Store a callback in an array with its associated
+	 * path (post method)
+	 *
+	 * @param $path
+	 * @param $callback
+	 *
+	 * does not return anything
+	 */
 	public function post($path, $callback)
 	{
 		if (!isset($this->routes['post'][$path]))
@@ -38,38 +67,82 @@ class Router
 			die("route $path is already used please update it");
 	}
 
+	/**
+	 * the heart of our routes
+	 * check if there is a callback for the current path
+	 * and execute it depends on its type
+	 * otherwise return 404 error
+	 * @return false|mixed|string|string[]
+	 */
 	public function resolve()
 	{
-//		echo $this->getUrl();
 		$path = $this->request->getPath();
-		$method = $this->request->getMethod();
+		$method = $this->request->Method();
 		$callback = $this->routes[$method][$path] ?? false;
 		if ($callback === false)
 		{
 			$this->response->setStatusCode(404);
-			return "Ops.. Page Not Found <h1>404</h1>";
+			return $this->renderContent(<<<TAG
+<title>404 Not Found</title><h1>404</h1><h2>Ops.. Page Not Found</h2>
+TAG
+);
 		}
 		if (is_string($callback))
 			return $this->renderView($callback);
-		return call_user_func($callback);
+		if (is_array($callback)) {
+			Application::$APP->controller = new $callback[0]();
+			$callback[0] = Application::$APP->controller;
+		}
+		return call_user_func($callback, $this->request);
 	}
 
-	public function renderView($view)
+	/**
+	 * mix the template with the asked $view
+	 *
+	 * @param $view string name of the view we want to compile with the template
+	 * @param array $params
+	 * @return false|string|string[]
+	 */
+	public function renderView(string $view, array $params = [])
 	{
+
 		$layout = $this->layoutContent();
-		$view = $this->renderOnlyView($view);
+		$view = $this->renderOnlyView($view, $params);
 		return str_replace('{{ body }}', $view, $layout);
 	}
 
+	/**
+	 * @param string $content content to be rendered in the template
+	 * @return false|string|string[]
+	 */
+	public function renderContent(string $content)
+	{
+		$layout = $this->layoutContent();
+		return str_replace('{{ body }}', $content, $layout);
+	}
+
+	/**
+	 * @return false|string the template content
+	 */
+
 	public function layoutContent()
 	{
+		$layout = Application::$APP->controller->layout;
 		ob_start();
-		include_once Application::$ROOT_DIR . "/views/layout/main.layout.php";
+		include_once Application::$ROOT_DIR . "/views/layout/$layout.layout.php";
 		return ob_get_clean();
 	}
 
-	protected function renderOnlyView($view)
+	/**
+	 * @param $view string the wanted view
+	 * @param array $params
+	 * @return string|null the view content
+	 */
+	protected function renderOnlyView(string $view, array $params): ?string
 	{
+		foreach ($params as $key => $param) {
+			$$key = $param;
+		}
 		ob_start();
 		include_once Application::$ROOT_DIR . "/views/$view.blade.php";
 		return ob_get_clean();
