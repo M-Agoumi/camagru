83313f5e62584dabff542e7176efe18513defbe0
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:06:40 +0100

    heaaader (navbar)

---
+++ views/layout/__header.php
@@ -0,0 +1,17 @@
+<header class="page-header">
+	<nav>
+		<ul>
+            <div>
+                <li><a class="active" href="/">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </div>
+            <!-- todo change this and put it in css file -->
+            <div style="float: right">
+                <li><a href="/login">Login</a></li>
+                <li><a href="/signup">register</a></li>
+            </div>
+		</ul>
+	</nav>
+</header>
