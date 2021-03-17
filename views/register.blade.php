7c0e11cf90fcc1a551df22754e9b93e184c33555
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:08:01 +0100

    register view

---
+++ views/register.blade.php
@@ -0,0 +1,13 @@
+<form method="POST" class="login-form">
+    <div class="row">
+        <div class="col-25">
+            <label for="email">Email</label>
+        </div>
+        <div class="col-75">
+            <input type="email" id="email" name="email" placeholder="Example@email.com">
+        </div>
+    </div>
+    <div class="row">
+        <input type="submit" value="Sign up">
+    </div>
+</form>
