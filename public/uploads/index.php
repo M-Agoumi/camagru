4ab2894bc2718da2f99645dc30650b0708f4fc60
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:05:44 +0100

    forbid direct access to this folder since it will contain the images of our application

---
+++ public/uploads/index.php
@@ -0,0 +1,6 @@
+<?php
+http_response_code(403);
+?>
+<title>403 Forbidden</title>
+<h1 style="color:darkred;text-align: center"> 403 Forbidden </h1>
+<h3 style="text-align: center">No direct access is allowed</h3>
