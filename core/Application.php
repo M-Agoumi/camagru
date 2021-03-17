2b63d4f86bb06de01112285f1a5d444faf91f3a5
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 17:59:00 +0100

    add controller getter and setter and document the script

--- core/Application.php
+++ core/Application.php
@@ -1,9 +1,25 @@
 <?php
+/* ************************************************************************************************ */
+/*                                                                                                  */
+/*                                                        :::   ::::::::   ::::::::  :::::::::::    */
+/*   Application.php                                   :+:+:  :+:    :+: :+:    :+: :+:     :+:     */
+/*                                                      +:+         +:+        +:+        +:+       */
+/*   By: magoumi <magoumi@student.1337.ma>             +#+      +#++:      +#++:        +#+         */
+/*                                                    +#+         +#+        +#+      +#+           */
+/*   Created: 2021/03/17 09:11:16 by magoumi         #+#  #+#    #+# #+#    #+#     #+#             */
+/*   Updated: 2021/03/17 09:11:16 by magoumi      ####### ########   ########      ###.ma           */
+/*                                                                                                  */
+/* ************************************************************************************************ */
+
 
 require_once 'Router.php';
 require_once 'Request.php';
-require_once 'Response.php';	
+require_once 'Response.php';
+require_once '../controller/Controller.php';
 
+/**
+ * Class Application
+ */
 class Application
 {
 	public static string $ROOT_DIR;
@@ -11,9 +27,14 @@ class Application
 	public ?Router $router = null;
 	public ?Request $request = null;
 	public ?Response $response = null;
+	public ?Controller $controller = null;
 
+	/**
+	 * Application constructor.
+	 * @param $rootPath string the root path of our application
+	 */
 
-	public function __construct($rootPath)
+	public function __construct(string $rootPath)
 	{
 		self::$ROOT_DIR = $rootPath;
 		self::$APP = $this;
@@ -22,9 +43,28 @@ class Application
 		$this->router = New Router($this->request, $this->response);
 	}
 
+	/**
+	 * calling the resolver method to handle our request
+	 */
 	public function run()
 	{
 		echo $this->router->resolve();
+	}
+
+	/**
+	 * @return Controller|null
+	 */
+	public function getController(): ?Controller
+	{
+		return $this->controller;
+	}
+
+	/**
+	 * @param Controller|null $controller
+	 */
+	public function setController(?Controller $controller): void
+	{
+		$this->controller = $controller;
 	}
 
 }
