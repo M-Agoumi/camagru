8873be974ff1fcbdab8feca6e10477a32410967d
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:04:46 +0100

    added the header :v and new routes

--- public/index.php
+++ public/index.php
@@ -1,36 +1,53 @@
 <?php
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   index.php                                          :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: magoumi <magoumi@student.1337.ma>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2021/03/16 17:05:43 by null          #+#    #+#                 */
-/*   Updated: 2021/03/16 17:05:43 by null          ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
+/* ************************************************************************************************ */
+/*                                                                                                  */
+/*                                                        :::   ::::::::   ::::::::  :::::::::::    */
+/*   index.php                                         :+:+:  :+:    :+: :+:    :+: :+:     :+:     */
+/*                                                      +:+         +:+        +:+        +:+       */
+/*   By: magoumi <magoumi@student.1337.ma>             +#+      +#++:      +#++:        +#+         */
+/*                                                    +#+         +#+        +#+      +#+           */
+/*   Created: 2021/03/16 17:05:43 by magoumi         #+#  #+#    #+# #+#    #+#     #+#             */
+/*   Updated: 2021/03/17 09:13:14 by magoumi      ####### ########   ########      ###.ma           */
+/*                                                                                                  */
+/* ************************************************************************************************ */
 
-
-/*
+/**
  * Getting global setting from the env file
  * enable error reporting if the env is dev
  */
+
 $config = parse_ini_file(__DIR__ ."/../.env");
 if (isset($config['env']) && $config['env'] === 'dev')
 	error_reporting(E_ALL ^ E_DEPRECATED);
 
-/*
+/**
  * require our app class the heart of our application
  */
 require_once __DIR__ . "/../core/Application.php";
 
-/*
+/**
  * creating a new instance of the Application class
  */
 $app = New Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
-$app->router->get('/contact', 'contact/index');
 
+/**
+ * include our controllers so we can send their methods
+ */
+
+include_once $app::$ROOT_DIR . "/controller/DefaultController.php";
+include_once $app::$ROOT_DIR . "/controller/AuthController.php";
+
+/**
+ * the routes of our application
+ */
+$app->router->get('/', [DefaultController::class, 'index']);
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'auth']);
+$app->router->get('/signup', [AuthController::class, 'register']);
+$app->router->post('/signup', [AuthController::class, 'signup']);
+
+/**
+ * run our application
+ */
 $app->run();
