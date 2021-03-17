62c972db2841f95986f9f503c2d4479c50b5a6b0
mohamed agoumi <agoumihunter@gmail.com>
2021-03-17 18:00:02 +0100

    updating getBody method, renaming getMethodt to method and adding isGet and isPost

--- core/Request.php
+++ core/Request.php
@@ -11,24 +11,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * Class Request
+ */
+
 class Request
 {
+	/**
+	 * @return string the current path of the application
+	 */
 	public function getPath(): string
 	{
 		$path = $_SERVER['REQUEST_URI'] ?? '/';
 		$position = strpos($path, '?');
 		if ($position === false)
 			return $path;
+
 		return substr($path, 0, $position);
 	}
 
-	public function getMethod()
+	/**
+	 * @return string the method of our request [get|post] in lowercase
+	 */
+	public function Method(): string
 	{
 		return strtolower($_SERVER['REQUEST_METHOD']);
 	}
 
-	public function getBody()
+	/**
+	 * @return bool
+	 */
+	public function isGet():bool
 	{
+		return $this->Method() === 'get';
+	}
 
+	/**
+	 * @return bool
+	 */
+	public function isPost():bool
+	{
+		return $this->Method() === 'post';
+	}
+
+	/**
+	 * handle the data coming from a form
+	 * @return array
+	 */
+	public function getBody(): array
+	{
+		$body = [];
+		if ($this->Method() === 'get') foreach ($_GET as $key => $value) {
+				$body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+			}
+		if ($this->Method() === 'post') foreach ($_POST as $key => $value) {
+			$body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+		}
+
+		return $body;
 	}
 }
