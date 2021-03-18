eaabb8295a10d5130abf202e9d6cb7a463109d51
mohamed agoumi <agoumihunter@gmail.com>
2021-03-18 13:28:55 +0100

    creating a form for the email validation code

--- views/messages/register_email.blade.php
+++ views/messages/register_email.blade.php
@@ -9,5 +9,11 @@ if (!isset($email))
 ?>
 <h1>One More Step Before Joining Us</h1>
 <p>We have sent a code to <?=$email?> enter it to continue your sign up</p>
-<input type="text">
+<form method="post" action="/register_step_2">
+	<input type="text" name="verification">
+	<?php if (!empty($error)): ?>
+		<?=$error?> <a href="#">resend code</a>
+		<!-- todo call method to resend email verification code -->
+	<?php endif; ?>
+</form>
 <!-- todo handle this page and code next steps -->
