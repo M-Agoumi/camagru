ec6cf0dac2da902f7599cd935d2963b9c005e8c2
M-Agoumi <agoumihunter@gmail.com>
2021-03-19 18:03:34 +0100

    language configs files

---
+++ translation/en.lang.php
@@ -0,0 +1,39 @@
+<?php
+/* ************************************************************************************************ */
+/*                                                                                                  */
+/*                                                        :::   ::::::::   ::::::::  :::::::::::    */
+/*   main.php                                          :+:+:  :+:    :+: :+:    :+: :+:     :+:     */
+/*                                                      +:+         +:+        +:+        +:+       */
+/*   By: magoumi <magoumi@student.1337.ma>             +#+      +#++:      +#++:        +#+         */
+/*                                                    +#+         +#+        +#+      +#+           */
+/*   Created: 2021/03/19 17:12:42 by magoumi         #+#  #+#    #+# #+#    #+#     #+#             */
+/*   Updated: 2021/03/19 17:12:42 by magoumi      ####### ########   ########      ###.ma           */
+/*                                                                                                  */
+/* ************************************************************************************************ */
+
+
+/**
+ * this is a language dictionary for multi languages
+ * this is file is the main language or you can call it the fallback in case of any error
+ */
+
+
+return array(
+    /**
+     * start of our dictionary
+     */
+
+    /** navbar */
+
+    'home' => 'Home',
+    'login' => 'Login',
+    'register' =>'Register',
+
+    /** error messages[form validation][models/Model.php] */
+
+    'RULE_REQUIRED' => 'This field is required',
+    'RULE_EMAIL' => 'This field must be a valid email',
+    'RULE_MIN' => 'Min length of this field is {min}',
+    'RULE_MAX' => 'Max length of this field is {max}',
+    'RULE_UNIQUE' => 'This field is used by someone else',
+);
