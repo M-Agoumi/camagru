5a65af7455471b1aacad395de1f687ce1425b316
mohamed agoumi <agoumi99@hotmail.com>
2021-03-21 22:07:42 +0100

    a custom 500 page yet to be developed [with the whole UI UX]

---
+++ views/error/__500.blade.php
@@ -0,0 +1,7 @@
+<title><?=$title ?? ''?></title>
+<h1>500</h1>
+<h3>Internal Server Error</h3>
+<p>
+	please try again later, if the problem still exists contact <a href="/contactus">us</a> with
+	the next error code: <?=$errorCode ?? ''?>
+</p>
