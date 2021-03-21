797f7ce0becc423be4f318f3d325c4c389a46e4b
mohamed agoumi <agoumi99@hotmail.com>
2021-03-21 22:09:08 +0100

    changing the from from HTML to PHP generated one

--- views/forms/register.blade.php
+++ views/forms/register.blade.php
@@ -1,26 +1,23 @@
 <?php
+	include_once Application::$ROOT_DIR . "/core/form/form.php";
 ?>
 <h1>You almost there just fill those fields and welcome onboard</h1>
 <?php
-    var_dump($user);
-?>
-<form method="POST" action="/registration" class="login-form">
-    <div class="row">
-        <div class="col-25">
-            <label for="email">Full Name</label>
-        </div>
-        <div class="col-75">
-            <input type="text" id="email" name="name" placeholder="John Dracula">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-25">
-            <label for="username">Username</label>
-        </div>
-        <div class="col-75">
-            <input type="text" id="username" name="username" placeholder="john_dr">
-        </div>
-    </div>
+ 	$form = Form::begin('registration', "POST", "login-form"); 
+		echo $form->field($user, 'name', 'Full Name', 'John Dracula')->required();
+		echo $form->field($user, 'email', 'Email')
+					->default('default@email.com')
+					->disabled()
+					->emailField();
+		echo $form->field($user, 'username')->required();
+		echo $form->field($user, 'password')->passwordField()->required();
+		echo $form->submite('Sign up');
+
+	Form::end();
+
+    // var_dump($user);
+	/*
+ <form method="POST" action="/registration" class="login-form">
     <div class="row">
         <div class="col-25">
             <label for="email">Email</label>
@@ -40,4 +37,5 @@
     <div class="row">
         <input type="submit" value="Sign up">
     </div>
-</form>
+</form> --}}
+*/
