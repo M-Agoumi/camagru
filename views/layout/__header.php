7d6111d68a32c0cd57624e30c4eb5a85e6eb30a5
mohamed agoumi <agoumihunter@gmail.com>
2021-03-27 19:04:20 +0100

    no more static links

--- views/layout/__header.php
+++ views/layout/__header.php
@@ -2,15 +2,15 @@
 	<nav>
 		<ul>
             <div>
-                <li><a class="active" href="/"><?= $this->lang('home');?></a></li>
+                <li><a class="active" href="<?=Application::path('home.index')?>"><?= $this->lang('home');?></a></li>
                 <li><a href="#news"><?= $this->lang('news');?></a></li>
                 <li><a href="#contact"><?= $this->lang('contact us  ');?></a></li>
                 <li><a href="#about"><?= $this->lang('about');?></a></li>
             </div>
             <!-- todo change this and put it in css file -->
             <div style="float: right">
-                <li><a href="/login"><?= $this->lang('login');?></a></li>
-                <li><a href="/signup"><?= $this->lang('register');?></a></li>
+                <li><a href="<?=Application::path('auth.login')?>"><?= $this->lang('login');?></a></li>
+                <li><a href="<?=Application::path('auth.singup')?>"><?= $this->lang('register');?></a></li>
             </div>
 		</ul>
 	</nav>
