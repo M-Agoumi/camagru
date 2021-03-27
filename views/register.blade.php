7701924f383d9f9ebf2d345dfd410d3466e4b54a
mohamed agoumi <agoumihunter@gmail.com>
2021-03-27 19:00:47 +0100

    remove useless html code

--- views/register.blade.php
+++ views/register.blade.php
@@ -10,16 +10,3 @@ echo $form->field($user, 'email')->emailField()->required()->setHolder('Example@
 echo $form->submit('Sign up');
 Form::end();
 ?>
-<form method="POST" class="login-form">
-    <div class="row">
-        <div class="col-25">
-            <label for="email">Email</label>
-        </div>
-        <div class="col-75">
-            <input type="email" id="email" name="email" placeholder="Example@email.com">
-        </div>
-    </div>
-    <div class="row">
-        <input type="submit" value="Sign up">
-    </div>
-</form>
