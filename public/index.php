4414e8946efe8eef02384e12441919c9be39283c
mohamed agoumi <agoumi99@hotmail.com>
2021-04-05 09:43:27 +0100

    remove routes and not needed includes
    since we are using autoloader now

--- public/index.php
+++ public/index.php
@@ -23,9 +23,12 @@ else
 	error_reporting(0);
 
 /**
- * require our app class the heart of our application
+ * require our autoloader
  */
-require_once __DIR__ . "/../core/Application.php";
+
+require_once "../__autoload.php";
+
+use core\Application;
 
 /**
  * creating a new instance of the Application class
@@ -33,31 +36,11 @@ require_once __DIR__ . "/../core/Application.php";
 
 $app = New Application(dirname(__DIR__));
 
-
 /**
- * include our controllers so we can send their methods
- * @VAR $ROOT_DIR string ../
+ * require the routes of our application
  */
 
-include_once $app::$ROOT_DIR . "/controller/DefaultController.php";
-include_once $app::$ROOT_DIR . "/controller/AuthController.php";
-
-/**
- * the routes of our application
- */
-$app->router->get('/', [DefaultController::class, 'index'])->name('home.index');
-$app->router->get('/login', [AuthController::class, 'login'])->name("auth.login");
-$app->router->post('/login', [AuthController::class, 'auth'])->name('auth.auth');
-$app->router->get('/signup', [AuthController::class, 'signup'])->name('auth.signup');
-$app->router->post('/signup', [AuthController::class, 'verifyEmail'])->name('auth.verifyEmail');
-$app->router->post('/register_step_2', [AuthController::class, 'register'])->name('auth.register');
-$app->router->get('/register_step_2', [AuthController::class, 'test'])->name('auth.test');
-$app->router->post('/registration', [AuthController::class, 'insertUser'])->name('auth.insertUser');
-$app->router->post('/testing', [AuthController::class, 'auth2'])->name('auth.auth2');
-$app->router->post('/logout', [AuthController::class, 'logout'])->name('app.logout');
-
-$app->router->get('/debugger', function (){return $_SESSION['email_code'];});
-
+require_once "../route.php";
 
 /**
  * run our application
