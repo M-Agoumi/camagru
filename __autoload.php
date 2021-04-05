b416c55b1ba390482aa9b2a1089ffdafc69d94b9
mohamed agoumi <agoumi99@hotmail.com>
2021-04-05 09:44:16 +0100

    autoloader 101, still open for more work

---
+++ __autoload.php
@@ -0,0 +1,32 @@
+<?php
+
+set_include_path(__DIR__.'/src/');
+
+spl_autoload_register('autoloader');
+
+function autoloader($class_name) {
+	
+	// echo "class name : $class_name <br>";
+    
+    if ($class_name == 'AuthController')
+    	$class_name = 'controller/AuthController';
+    
+    //    echo "Include path: " . get_include_path() . "<br>";
+    
+	$file_name = get_include_path() .$class_name . '.php';
+
+	//    echo "File name : $file_name <br>";
+ 
+	$file_name = str_replace("\\", "/", $file_name);
+ 
+	if (file_exists($file_name)) {
+
+        // echo "class include ".$class_name . "<br>";
+  
+		require_once($file_name);
+    } else {
+        echo "file not found: " . $file_name;
+    }
+
+	//    echo "<br><br>";
+}
