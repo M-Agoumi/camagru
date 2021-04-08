be6c32f9193e7a37a4f80508fd2093e82885a7ea
M-Agoumi <agoumihunter@gmail.com>
2021-04-08 16:27:03 +0100

    add camera control function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,3 +8,23 @@ async function dismissMessage() {
     await sleep(1000);
     source.style.display = "none";
 }
+
+async function getWebCam() {
+    try {
+        const videoSrc = await navigator.mediaDevices.getUserMedia({video: true});
+        let video = document.getElementById("video");
+        video.srcObject = videoSrc;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const capture = document.getElementById('capture');
+const canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+capture.addEventListener("click", function (){
+    context.drawImage(video, 0,0,650, 490);
+});
+
+getWebCam();
