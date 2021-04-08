8024a013df8b16b25a66e65dfa853a89fea8d446
M-Agoumi <agoumihunter@gmail.com>
2021-04-08 16:27:31 +0100

    camera view page

--- views/pages/camera.blade.php
+++ views/pages/camera.blade.php
@@ -1,2 +1,11 @@
 <h1>Smile to the camera</h1>
 
+<div>
+    <div>
+        <video autoplay="autoplay" id="video"></video>
+    </div>
+    <div>
+        <canvas id="canvas" width="650" height="490"></canvas>
+    </div>
+    <div id="capture">capture</div>
+</div>
