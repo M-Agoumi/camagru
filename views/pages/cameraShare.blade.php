ee3bd1e1c70c44616ee6f8d2226ff957b468e553
M-Agoumi <agoumihunter@gmail.com>
2021-04-16 14:23:18 +0000

    Post picture page

---
+++ views/pages/cameraShare.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+use core\Form\Form;
+use core\Application;
+
+/** @var  $post \models\Post */
+
+$form = Form::begin(Application::path('camera.share'), "POST", "login-form");
+?>
+    <img src="<?= Application::$APP->request->getBody()['picture']?>" alt="">
+    <?= $form->field($post, 'picture')->hiddentField()?>
+    <?= $form->field($post, 'title')->setHolder('If you want it to have one :D') ?>
+    <?= $form->text($post, 'comment')->setHolder('beautiful day in the beach?') ?>
+
+    <?= $form->submit('Post'); ?>
+<?php Form::end();
