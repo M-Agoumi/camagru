0c68a646807e7e170fa8b5eca5e7af180edc3bbc
M-Agoumi <agoumihunter@gmail.com>
2021-04-16 14:14:20 +0000

    putting js code here so we dont ask for camera in every page

--- views/pages/camera.blade.php
+++ views/pages/camera.blade.php
@@ -1,11 +1,34 @@
 <h1>Smile to the camera</h1>
 
-<div>
-    <div>
+<div class="camera_container">
+    <div class="camera" id="camera">
         <video autoplay="autoplay" id="video"></video>
+        <div class="capture" onclick="capture()">capture</div>
+    </div>
+    <div class="picture" id="picture">
+        <canvas id="canvas" width="650" height="490"></canvas>
+        <div class="capture" onclick="save()">confirm image</div>
+        <div class="capture" onclick="getWebCam()">Retake</div>
     </div>
     <div>
-        <canvas id="canvas" width="650" height="490"></canvas>
+        <form method="post">
+            <input type="hidden" name='picture' id="inputPicture">
+            <input type="submit" value="save">
+        </form>
     </div>
-    <div id="capture">capture</div>
 </div>
+
+<script>
+    async function getWebCam() {
+        try {
+            const videoSrc = await navigator.mediaDevices.getUserMedia({video: true});
+            let video = document.getElementById("video");
+            video.srcObject = videoSrc;
+            document.getElementById('picture').style.display = 'none';
+            document.getElementById('camera').style.display = 'block';
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    getWebCam();
+</script>
