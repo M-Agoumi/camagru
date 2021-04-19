e7b32b5ddfb16444a7aeeca8a5dcf0cb81b88f11
M-Agoumi <agoumihunter@gmail.con>
2021-04-19 17:07:01 +0000

    apply filters to the image

--- views/pages/camera.blade.php
+++ views/pages/camera.blade.php
@@ -10,8 +10,44 @@
         <div class="capture" onclick="save()">confirm image</div>
         <div class="capture" onclick="getWebCam()">Retake</div>
         <div class="filters">
-            <span>brightness <span onclick="brightnessUp()">(+)</span><span onclick="brightnessDown()">(-)</span></span>
-        </div>
+			<span onclick="clearFilters()">Original</span>
+            <span>
+				brightness:
+				<span onclick="brightnessToggle()">bright</span>
+				<span onclick="brightnessUp()">(+)</span>
+				<span onclick="brightnessDown()">(-)</span>
+			</span>
+			<span>
+				grayscale:
+				<span onclick="grayscaleToggle()">gray</span>
+				<span onclick="grayscaleUp()">(+)</span>
+				<span onclick="grayscaleDown()">(-)</span>
+			</span>
+			<span>
+				blur:
+				<span onclick="blurToggle()">blur</span>
+				<span onclick="blurUp()">(+)</span>
+				<span onclick="blurDown()">(-)</span>
+			</span>
+			<span>
+				contrast:
+				<span onclick="contrastToggle()">contrast</span>
+				<span onclick="contrastUp()">(+)</span>
+				<span onclick="contrastDown()">(-)</span>
+			</span>
+			<span>
+				hue:
+				<span onclick="hueToggle()">hue</span>
+				<span onclick="hueUp()">(+)</span>
+				<span onclick="hueDown()">(-)</span>
+			</span>
+			<span>
+				invert:
+				<span onclick="invertToggle()">invert</span>
+				<span onclick="invertUp()">(+)</span>
+				<span onclick="invertDown()">(-)</span>
+			</span>
+		</div>
     </div>
     <div>
         <form method="post">
