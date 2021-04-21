4322bd25e8a6320879c716c3d5eb135a1688a096
M-Agoumi <agoumihunter@gmail.com>
2021-04-21 16:29:11 +0000

    return server port

--- bin/scripts/server_status.sh
+++ bin/scripts/server_status.sh
@@ -6,7 +6,7 @@ if [ -f $FILE ];then
     _pid=$(cat $FILE);
     _ps=$(ps -p "$_pid" | wc -l)
     if [ "$_ps" = 2 ]; then
-      echo "1";
+      ss -tnlp | grep "$_pid" | cut -d ":" -f2 | cut -d " " -f1
     else
       echo "0";
     fi
