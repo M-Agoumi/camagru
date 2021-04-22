c6bff2abb65a66bc828680d64aec2648bb39bdc8
M-Agoumi <agoumihunter@gmail.com>
2021-04-22 17:43:28 +0000

    update profile view page

---
+++ views/pages/updateProfile.blade.php
@@ -0,0 +1,13 @@
+<?php
+use core\Application;
+use core\Form\Form;
+
+/** @var $user \models\User */
+?>
+<h1>Edit profile <?=$user->name?></h1>
+<?php $form = Form::begin(Application::path('user.update'), "POST", "update-form");
+        echo $form->field($user, 'name');
+        echo $form->field($user, 'username');
+        echo $form->field($user, 'email');
+        echo $form->submit('edit');
+    Form::end();
