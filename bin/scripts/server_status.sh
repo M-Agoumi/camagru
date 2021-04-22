227f463adc0147c81d3ea455c58b88addd8fe7e0
M-Agoumi <agoumihunter@gmail.com>
2021-04-22 17:38:54 +0000

    command alternative

--- bin/scripts/server_status.sh
+++ bin/scripts/server_status.sh
@@ -7,6 +7,7 @@ if [ -f $FILE ];then
     _ps=$(ps -p "$_pid" | wc -l)
     if [ "$_ps" = 2 ]; then
       ss -tnlp | grep "$_pid" | cut -d ":" -f2 | cut -d " " -f1
+      #ss -tnlp | grep "$_pid" | sed "s/$_pid //" | tr -d ' ' | cut -b 22-25
     else
       echo "0";
     fi
