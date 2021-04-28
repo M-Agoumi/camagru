1a701e9dc9b21ddd7ad2d513eafb6a4307e83182
M-Agoumi <agoumihunter@gmail.com>
2021-04-28 15:48:41 +0000

    add restore password link

--- views/login.blade.php
+++ views/login.blade.php
@@ -7,10 +7,13 @@
 ?>
 <h1>Sign In</h1>
 <?php
- 	$form = Form::begin(Application::path('auth.auth2'), "POST", "login-form");
+ 	$form = Form::begin(Application::path('auth.auth2') . "?ref=". $_GET['ref'], "POST", "login-form");
 		echo $form->field($user, 'username', 'Username OR Email')->required()->setHolder('John Dracula');
 		echo $form->field($user, 'password')->passwordField();
         echo $form->submit('Sign up');
 
     Form::end();
 ?>
+<div class="center">
+    <a href="<?=Application::path('auth.restore')?>">restore password</a>
+</div>
