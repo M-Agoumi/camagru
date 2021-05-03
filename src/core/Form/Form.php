16edb1341a53464d87f916f684710fe39113efa4
M-Agoumi <agoumihunter@gmail.com>
2021-05-03 17:45:51 +0000

    add extra variable for whatever you want to add like events handlers

--- src/core/Form/Form.php
+++ src/core/Form/Form.php
@@ -27,12 +27,12 @@ class Form
 	 * @param string $class
 	 * @return Form instance so we can access it's method to generate a form
 	 */
-	public static function begin(string $action = '', string $method = '', string $class = ''): Form
+	public static function begin(string $action = '', string $method = 'POST', string $class = '', string $event = null): Form
 	{
 		if (!$action)
 			$action = Application::$APP->request->getPath();
 		Application::$APP->session->generateCsrf();
-		echo sprintf('<form action="%s" method="%s" class="form %s">', $action, $method, $class);
+		echo sprintf('<form action="%s" method="%s" class="form %s" %s>', $action, $method, $class, $event);
 		echo sprintf('<input type="hidden" name="__csrf" value="%s">', Application::$APP->session->getCsrf());
 		return new Form();
 	}
@@ -68,9 +68,9 @@ class Form
 	 * @param string $value
 	 * @return string
 	 */
-	public function submit(string $value): string
+	public function submit(string $value, string $extra = ''): string
 	{
-		return '<div class="row"><input type="submit" value="' . $value . '"></div>';
+		return '<div class="row"><input type="submit" ' . $extra . ' value="' . $value . '"></div>';
 	}
 
 }
