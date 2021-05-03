bd4daa285825c5050021e846ccb4369446fe6053
M-Agoumi <agoumihunter@gmail.com>
2021-05-03 17:46:53 +0000

    like entity and its migration

---
+++ migrations/mg0004_creating_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use core\Application;
+
+class mg0004_creating_likes_table
+{
+	public function up()
+	{
+		$db = Application::$APP->db;
+
+		$db->pdo->exec("CREATE TABLE IF NOT EXISTS `likes` (
+					 	`id`         int NOT NULL AUTO_INCREMENT ,
+					 	`post`       int NOT NULL ,
+					 	`user`       int NOT NULL ,
+					 	`type`       tinyint NOT NULL default 0,
+				 		`status`     tinyint NOT NULL default 0,
+					 	`created_at` timestamp NOT NULL ,
+					 	`updated_at` timestamp NULL ,
+					 	PRIMARY KEY (`id`),
+                       	KEY `fkIdx_38` (`post`),
+						CONSTRAINT `FK_37` FOREIGN KEY `fkIdx_38` (`post`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
+						KEY `fkIdx_41` (`user`),
+						CONSTRAINT `FK_40` FOREIGN KEY `fkIdx_41` (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+						);");
+	}
+
+	public function down()
+	{
+		$db = Application::$APP->db;
+
+		$db->pdo->exec("DROP TABLE IF EXISTS likes");
+	}
+}
---
+++ src/models/Likes.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace models;
+
+
+use core\Db\DbModel;
+
+class Likes extends DbModel
+{
+
+	public ?int $id = null;
+	public ?int $post = null;
+	public ?int $user = null;
+	public ?int $status = null;
+
+
+	public function tableName(): string
+	{
+		return "likes";
+	}
+
+	public function attributes(): array
+	{
+		return [
+			'post',
+			'user',
+			'type',
+			'status'
+		];
+	}
+
+	public function primaryKey(): string
+	{
+		return 'id';
+	}
+
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	/**
+	 * the rules should be respect by each child model
+	 * @return array
+	 */
+	public function rules(): array
+	{
+		// TODO: Implement rules() method.
+	}
+}
