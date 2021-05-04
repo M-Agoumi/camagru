a6306c5dd5f49b46831e8dccbc1c6bba314b1874
M-Agoumi <agoumihunter@gmail.com>
2021-05-04 21:28:16 +0000

    Like button works

--- src/controller/PostController.php
+++ src/controller/PostController.php
@@ -4,10 +4,12 @@
 namespace controller;
 
 
+use core\Application;
 use core\Exception\ForbiddenException;
 use core\Exception\NotFoundException;
 
 use core\Request;
+use models\Likes;
 use models\Post;
 
 class PostController extends Controller
@@ -25,10 +27,46 @@ class PostController extends Controller
 		throw New NotFoundException();
 	}
 
-	public function like(string $slug, Request $request): string
+	public function like(string $id, Request $request): string
 	{
-		if ($request->isPost())
-			return "liking $slug";
+		if ($request->isPost()) {
+			if (Application::isGuest())
+				return "-1";
+
+			$post = New Post();
+			$post = $post->getOneBy($id);
+
+			if ($post) {
+				$likes = New Likes();
+				$liked = $likes->findOne([
+					'user' => Application::$APP->user->getId(),
+					'post' => $post->id,
+					]
+				);
+				if ($liked) {
+					if ($liked->status) {
+						$liked->status = 0;
+						if ($liked->update())
+							echo "0";
+					} else {
+						$liked->status = 1;
+						if ($liked->update())
+							echo "1";
+					}
+				} else {
+					$likes->post = $id;
+					$likes->user = Application::$APP->user->getId();
+					$likes->status = 0;
+					$likes->type = 0;
+					if ($likes->save())
+						echo "1";
+				}
+			} else
+				echo "Post not found maybe got deleted";
+
+
+			return "\n";
+		}
 		throw New ForbiddenException();
 	}
 }
