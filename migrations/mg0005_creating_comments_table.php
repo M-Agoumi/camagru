90a7b490decbd80cb534b9376cf8456beadc75ea
M-Agoumi <agoumihunter@gmail.com>
2021-05-18 23:39:54 +0100

    comments and comments_reacts tables

---
+++ migrations/mg0005_creating_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use core\Application;
+
+class mg0005_creating_comments_table
+{
+	public function up()
+	{
+		$db = Application::$APP->db;
+
+		$db->pdo->exec("CREATE TABLE `comments` (
+							 `id`         int NOT NULL AUTO_INCREMENT ,
+							 `post`       int NOT NULL ,
+							 `user`       int NOT NULL ,
+							 `content`    text NOT NULL ,
+							 `status`     tinyint NOT NULL default 0,
+							 `created_at` timestamp NOT NULL ,
+							 `updated_at` timestamp NULL ,
+							
+							PRIMARY KEY (`id`),
+							KEY `fkIdx_51` (`post`),
+							CONSTRAINT `FK_50` FOREIGN KEY `fkIdx_51` (`post`) REFERENCES `posts` (`id`),
+							KEY `fkIdx_54` (`user`),
+							CONSTRAINT `FK_53` FOREIGN KEY `fkIdx_54` (`user`) REFERENCES `users` (`id`)
+							);
+						");
+	}
+
+	public function down()
+	{
+		$db = Application::$APP->db;
+
+		$db->pdo->exec("DROP TABLE IF EXISTS comments");
+	}
+}
---
+++ migrations/mg0006_creating_comment_reacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use core\Application;
+
+class mg0006_creating_comment_reacts_table
+{
+	public function up()
+	{
+		$db = Application::$APP->db;
+
+		$db->pdo->exec("CREATE TABLE `comment_reacts` (
+						 `id`         int NOT NULL AUTO_INCREMENT ,
+						 `comment`    int NOT NULL ,
+						 `user`       int NOT NULL ,
+						 `type`       tinyint NOT NULL ,
+						 `status`     tinyint NOT NULL default 0,
+						 `created_at` timestamp NOT NULL ,
+						 `updated_at` timestamp NULL ,
+						
+						PRIMARY KEY (`id`),
+						KEY `fkIdx_64` (`comment`),
+						CONSTRAINT `FK_63` FOREIGN KEY `fkIdx_64` (`comment`) REFERENCES `comments` (`id`),
+						KEY `fkIdx_67` (`user`),
+						CONSTRAINT `FK_66` FOREIGN KEY `fkIdx_67` (`user`) REFERENCES `users` (`id`)
+						);
+					");
+	}
+
+	public function down()
+	{
+		$db = Application::$APP->db;
+
+		$db->pdo->exec("DROP TABLE IF EXISTS comment_reacts");
+	}
+}
