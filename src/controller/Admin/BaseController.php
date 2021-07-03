8680ca6b40cd9eb13f3427a6e6b67d2eee27592c
M-Agoumi <agoumihunter@gmail.com>
2021-07-04 00:39:03 +0100

    base admin controller implementing admin middleware

---
+++ src/controller/Admin/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace controller\Admin;
+
+
+use controller\Controller;
+use Middlewares\AdminMiddleware;
+use Middlewares\AuthMiddleware;
+
+class BaseController extends Controller
+{
+	public function __construct()
+	{
+		$this->registerMiddleware(New AuthMiddleware([]));
+		$this->registerMiddleware(New AdminMiddleware([]));
+	}
+}
