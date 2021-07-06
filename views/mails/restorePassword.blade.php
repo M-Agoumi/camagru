dd280412f5866081dd151d8152fe3af2320a11cc
M-Agoumi <agoumihunter@gmail.com>
2021-07-06 02:00:03 +0100

    mail view and layout for restore password

---
+++ views/mails/restorePassword.blade.php
@@ -0,0 +1,3 @@
+<div style="background: darkgray; width: 200px; text-align: center; margin: auto; padding: 15px 20px; border-radius: 5px">
+    <a style="text-decoration: none; color: chartreuse;" href='http://localhost:<?=$port?>/verify-token/<?=$token?>'>reset your password</a>
+</div>
