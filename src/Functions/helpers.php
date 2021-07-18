0ac792b242c4cc7f21d3be9b6b9cc35ca415de7e
M-Agoumi <agoumihunter@gmail.com>
2021-07-18 02:11:59 +0100

    helpers function to help shorten the code in our application

--- src/Functions/helpers.php
+++ src/Functions/helpers.php
@@ -1,8 +1,50 @@
 <?php
 
+use core\Application;
 use core\View;
 
-function asset($asset)
+/**
+ * @param $asset
+ * @return string
+ */
+function asset($asset): string
 {
 	return View::asset($asset);
+}
+
+function render(string $view, array $params = [], array $layParams = [])
+{
+	return Application::$APP->view->renderView($view, $params, $layParams);
+}
+
+function route($path): string
+{
+	return Application::path($path);
+}
+
+/** convert from timestamp to human readable time
+ * @param $time
+ * @return string
+ */
+function humanTiming($time): string
+{
+
+	$time = time() - $time; // to get the time since that moment
+	$time = ($time < 1) ? 1 : $time;
+	$tokens = array(
+		31536000 => 'year',
+		2592000 => 'month',
+		604800 => 'week',
+		86400 => 'day',
+		3600 => 'hour',
+		60 => 'minute',
+		1 => 'second'
+	);
+
+	foreach ($tokens as $unit => $text) {
+		if ($time < $unit) continue;
+		$numberOfUnits = floor($time / $unit);
+		return $numberOfUnits . ' ' . $text . (($numberOfUnits > 1) ? 's' : '');
+	}
+	return "0";
 }
