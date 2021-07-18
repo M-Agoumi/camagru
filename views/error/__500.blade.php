e5aedaae3077c60141660a4758344621ac2deb26
M-Agoumi <agoumihunter@gmail.com>
2021-07-18 01:54:16 +0100

    UX improvements

--- views/error/__500.blade.php
+++ views/error/__500.blade.php
@@ -1,7 +1,6 @@
-<title><?=$title ?? ''?></title>
 <h1>500</h1>
-<h3 class="center">Internal Server Error</h3>
+<h3 style="text-align: center">Internal Server Error</h3>
 <p>
-	please try again later, if the problem still exists contact <a href="/contactus">us</a> with
-	the next error code: <?=$errorCode ?? ''?>
+	please try again later, if the problem still exists contact <a href="<?=route('contact.us')?>">us</a> with
+	the next error code: <?=$errorCode ?? '500'?> and description of how you reached this page
 </p>
