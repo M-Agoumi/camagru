1c59e9ab9e756ec9da71faa9760d336d554d5010
M-Agoumi <agoumihunter@gmail.com>
2021-07-18 02:23:08 +0100

    email view for emails verification code

---
+++ views/mails/verifyEmail.blade.php
@@ -0,0 +1,3 @@
+<div style="background: darkgray; width: 200px; text-align: center; margin: auto; padding: 15px 20px; border-radius: 5px">
+    <a style="text-decoration: none; color: chartreuse;" href='http://localhost:<?=$port ?? ''?>/verify-email/<?=$token??''?>'>Verify Your Email</a>
+</div>
