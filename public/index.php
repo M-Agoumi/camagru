a9fa58b96625048382715c5a9d2c22bf2c6eb797
M-Agoumi <agoumihunter@gmail.com>
2021-07-18 02:13:34 +0100

    add statics for developers

--- public/index.php
+++ public/index.php
@@ -7,18 +7,20 @@
 /*   By: magoumi <magoumi@student.1337.ma>             +#+      +#++:      +#++:        +#+         */
 /*                                                    +#+         +#+        +#+      +#+           */
 /*   Created: 2021/03/16 17:05:43 by magoumi         #+#  #+#    #+# #+#    #+#     #+#             */
-/*   Updated: 2021/03/17 09:13:14 by magoumi      ####### ########   ########      ###.ma           */
+/*   Updated: 2021/07/17 21:52:45 by magoumi      ####### ########   ########      ###.ma           */
 /*                                                                                                  */
 /* ************************************************************************************************ */
 
 /**
  * coded on php 7.4.3 environment compatible with php 8.0*
  * (*)has not been tested on it yet
- *
- * let make our code professional by declaring strict Type
  */
 
-//declare(strict_types=1);
+/**
+ * start counting execution time
+ */
+
+$start = microtime(true);
 
 /**
  * set cookies to httponly to protect from XSS
@@ -43,6 +45,26 @@ else
 require_once "../__autoload.php";
 
 use core\Application;
+
+/**
+ * register shutdown function
+ */
+
+function shutdown($start)
+{
+	$time_elapsed_secs = microtime(true) - $start;
+	$load = sys_getloadavg();
+
+	if (Application::$APP::$ENV['env'] == 'dev') {
+		echo '<script>';
+			echo 'console.log("execution time ' . round($time_elapsed_secs, 3) . 's");' . PHP_EOL;
+			echo 'console.log("Peak memory: ' . round(memory_get_peak_usage() / 1024) . 'KB");';
+			echo 'console.log("CPU Usage: ' . $load[0] . '%");';
+		echo '</script>', PHP_EOL;
+	}
+}
+
+register_shutdown_function('shutdown', $start);
 
 /**
  * creating a new instance of the Application class
