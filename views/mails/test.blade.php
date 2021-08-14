efa69049247dbec82697fc7217c2125b888b6e36
mohamed agoumi <agoumihunter@gmail.com>
2021-08-14 22:35:01 +0100

    test mail blade

---
+++ views/mails/test.blade.php
@@ -0,0 +1,5 @@
+<h1>this is a test to <?=$receiver?></h1>
+<p>
+        this is a test sent to you in order to check our mailing system, and see if it's working properly, sorry for any
+    inconvenient
+</p>
