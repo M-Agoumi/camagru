d7b6481f6b45a88508a37a956cbacf2e7371b384
m-agoumi <agoumihunter@gmail.com>
2021-09-19 01:03:18 +0100

    add slugify method

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -90,4 +90,31 @@ abstract class Controller
 	    } else
     	    return $this->mailer($to, $subject, $content, $headers);
     }
+
+	public function slugify($text, string $divider = '-'): string
+	{
+		// replace non letter or digits by divider
+		$text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+		// transliterate
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		// trim
+		$text = trim($text, $divider);
+
+		// remove duplicate divider
+		$text = preg_replace('~-+~', $divider, $text);
+
+		// lowercase
+		$text = strtolower($text);
+
+		if (empty($text)) {
+			return 'n-a';
+		}
+
+		return $text;
+	}
 }
