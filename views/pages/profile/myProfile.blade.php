3b8090c6f1b369f47de60f9940be55e65a46f392
yassineelidrissi <yassineelidrissi02@gmail.com>
2021-09-21 23:15:20 +0100

    Recreate the profile page structure

--- views/pages/profile/myProfile.blade.php
+++ views/pages/profile/myProfile.blade.php
@@ -4,14 +4,37 @@
 	<?php
 	/** @var $user User */
 	?>
-	<h1>Welcome back <?=$user->name?></h1>
-	<p>Name: <?=$user->name?></p>
-	<p>Username: <?=$user->username?></p>
-	<p>EmailL: <?=$user->email?></p>
-	<p>Status: <?=$user->status ? 'Activated' : 'Not Activated'?></p>
-	<p>Joined: <?=explode(" ", $user->created_at)[0]?></p>
-	<a href="<?=route('user.edit')?>">Edit Information</a>
-	<a href="<?=route('user.preferences')?>">update preferences</a>
+
+	<div class="my-profile">
+		<div class="profile-cover"></div>
+		<div class="profile-logo"></div>
+		{{-- <h1>Welcome back <?=$user->name?></h1> --}}
+		<div class="profile-user-info">
+			<p><?=$user->name?></p>
+			<p>Username: <?=$user->username?></p>
+		</div>
+		<div class="profile-parent">
+			<div class="profile-info">
+				<div class="user-info">
+					<p><i class="fa fa-envelope"></i> <?=$user->email?></p>
+					<p><i class="fa fa-check"></i> <?=$user->status ? 'Activated' : 'Not Activated'?></p>
+					<p><i class="fa fa-calendar"></i> <?=explode(" ", $user->created_at)[0]?></p>
+				</div>
+				<div class="buttons">
+					<a href="<?=route('user.edit')?>">Edit Information</a>
+					<a href="<?=route('user.preferences')?>">update preferences</a>
+				</div>
+			</div>
+			<div class="profile-posts">
+				<h3>Posts</h3>
+				<div class="post"></div>
+				<div class="post"></div>
+				<div class="post"></div>
+				<div class="post"></div>
+			</div>
+		</div>
+	</div>
+
 @endsection
 
 
