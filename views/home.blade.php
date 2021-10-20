03012da27a7e76f9a3de4353534d726f71d62f1a
m-agoumi <agoumihunter@gmail.com>
2021-10-20 02:29:30 +0100

    Ajaxify posts and add infinite scroll

--- views/home.blade.php
+++ views/home.blade.php
@@ -3,46 +3,103 @@
 @section('content')
     <h1><?=lang('home')?></h1>
     <div class="masonry-container">
-        <!-- =============================================== -->
-        <div class="gal-one">
-            <?php foreach ($posts as $post): ?>
-                <div class="panel">
-                    <a href="/post/{{ post['slug'] }}">
-                        <div class="panel-wrapper">
-                            <div class="panel-overlay">
-                                <div class="panel-text">
-                                    <div class="panel-title">{{ post['title'] }}</div>
-                                    <div class="panel-tags">
-                                        <?php
-                                            $tags = $postModule->hashtag($post['comment']);
-                                        ?>
-                                        @if ($tags):
-	                                        <span class="tag-icon">
-	                                            <img class="tag-icon-img" src="/uploads/tag-icon.svg" alt=""/>
-	                                        </span>
-	                                        <span class="tags-list"><?=$postModule->hashtag($post['comment']) ?></span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <img class="panel-gradient" src="/uploads/base-gradient.png" alt=""/>
-                                <img class="panel-vingette" src="/uploads/darken-gradient.png" alt=""/>
-                            </div>
-                            <img class="panel-img" src="/uploads/{{ post['picture'] }}" alt=""/>
-                        </div>
-                    </a>
-                </div>
-            <?php endforeach; ?>
-        </div>
-        <!-- =============================================== -->
-	    <div style="color: #000000; text-align: center">
-		    pages:
-		    <?php foreach($postModule->pages() as $page): ?>
-				<?php if (is_array($page)): ?>
-					<a href="?page={{ page['active'] }}" class="active" style="color: #FF0000;text-decoration: underline">{{ page['active'] }}</a>
-				<?php else: ?>
-		            <a href="?page={{ page }}" style="color: #ff4e00">{{ page }}</a>
-	            <?php endif;?>
-		    <?php endforeach; ?>
-	    </div>
+	    <div style="text-align: center" id="loading">Content loading...</div>
     </div>
+    <div class="gal-one" id="gallery">
+
+    </div>
+	<script>
+		let shouldRemove = true;
+        let page = 1;
+        let firstRun = true;
+        let reachedEnd = false;
+        window.onload = loadPosts();
+
+        function loadPosts() {
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'http://camagru.io:8080/posts');
+
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = () => {
+                let posts = JSON.parse(xhr.responseText);
+
+                if (shouldRemove) {
+                    shouldRemove = false;
+                    document.getElementById('loading').remove();
+                }
+
+                if (posts.length === 0) {
+                    if (!reachedEnd) {
+                        reachedEnd = true;
+                        document.getElementById('gallery').insertAdjacentHTML('beforeend', "<h1 class='endOfPosts'>there is no more posts</h1>");
+                    }
+                } else {
+                    for (var i in posts)
+                    {
+                        let id = Math.random().toString(36).slice(2);
+                        let post = '';
+                        post += '<div class="panel">';
+                        post += '<a href="/post/' + posts[i].slug + '">';
+                        post += '<div class="panel-wrapper">';
+                        post += '<div class="panel-overlay">';
+                        post += '<div class="panel-text">';
+                        post += '<div class="panel-title">' + posts[i].title + '</div>';
+                        post += '<div class="panel-tags">';
+                        post += '<span class="tag-icon">';
+                        post += '<img class="tag-icon-img" src="/uploads/tag-icon.svg" alt=""/>';
+                        post += '</span>';
+                        post += '<span class="tags-list">' + posts[i].hashtags + '</span>'
+                        post += '</div>';
+                        post += '</div>';
+                        post += '<img class="panel-gradient" src="/uploads/base-gradient.png" alt=""/>';
+                        post += '<img class="panel-vingette" src="/uploads/darken-gradient.png" alt=""/>';
+                        post += '</div>';
+                        post += '<img class="panel-img" id=' + id + ' alt="' + posts[i].title + '"/>';
+                        post += '</div>';
+                        post += '</a>';
+                        post += '</div>';
+
+                        // document.getElementById('gallery').appendChild(post);
+                        document.getElementById('gallery').insertAdjacentHTML('beforeend', post);
+                        loadImage(posts[i].picture, id);
+                    }
+                    page++;
+                }
+            }
+
+            xhr.send("page=" + page);
+        }
+
+        function loadImage(url, id)
+        {
+	        document.getElementById(id).src = '/uploads/' + url;
+        }
+
+        function getScrollPercent() {
+            var h = document.documentElement,
+                b = document.body,
+                st = 'scrollTop',
+                sh = 'scrollHeight';
+            return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+        }
+
+        document.addEventListener('DOMContentLoaded', function(e) {
+            document.addEventListener('scroll', function(e) {
+                // let documentHeight = document.body.scrollHeight;
+                // let currentScroll = window.scrollY + window.innerHeight;
+                // // When the user is [modifier]px from the bottom, fire the event.
+                // let modifier = 10;
+                // if(currentScroll + modifier > documentHeight) {
+                //     console.log('You are at the bottom!');
+                //     loadPosts();
+                // }
+	            if (firstRun)
+                    firstRun = false;
+                else
+	                if (getScrollPercent() === 100)
+                        loadPosts();
+            })
+        })
+	</script>
 @endsection
