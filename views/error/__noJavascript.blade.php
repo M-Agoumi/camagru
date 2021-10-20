5cd17e38594dc717e03b80077ca6efd51ef66994
m-agoumi <agoumihunter@gmail.com>
2021-10-20 02:31:26 +0100

    first run of the framework message

---
+++ views/error/__noJavascript.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Javascript is disabled</title>
+</head>
+<body style="background: #FFEA61">
+<div style="position: absolute; top:50%;left: 50%;transform: translate(-50%, -50%); text-align: center;">
+	<h1 style="color: #fe0000; font-size: 62px">Javascript is disabled</h1>
+	<p style="font-size: 32px">
+		this web application depends on javascript, and it can't work without it, please allow us to use <strong>Javascript</strong>
+	</p>
+</div>
+</body>
+</html>
