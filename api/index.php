7a544c616617cf62baeb2f75e4553f19c98f08fb
m-agoumi <agoumihunter@gmail.com>
2021-10-20 02:23:55 +0100

    adapt to api app

--- api/index.php
+++ api/index.php
@@ -12,10 +12,19 @@
 /* ************************************************************************************************ */
 
 /**
- * let make our code professional by declaring strict Type
+ * coded on php 7.4.3 environment compatible with php 8.0*
+ * (*)has not been tested on it yet
+ * check if php version is meet
  */
 
-//declare(strict_types=1);
+if (version_compare(phpversion(), '7.4.0', '<='))
+	die('php version is too old, this scripts require php >=  v7.4');
+
+/**
+ * start counting execution time
+ */
+
+$start = microtime(true);
 
 /**
  * set cookies to httponly to protect from XSS
@@ -23,15 +32,44 @@
 ini_set('session.cookie_httponly', 1);
 
 /**
+ * check if maintenance mode is on
+ */
+if (file_exists("../var/cache/maintenance_on")) {
+	$allowed = unserialize(trim(file_get_contents("../var/cache/maintenance_on")));
+
+	if (!in_array($_SERVER['REMOTE_ADDR'], $allowed) && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+		include("../views/error/maintenance.html");
+		exit(0);
+	}
+	echo '<h1 style="font-size: 34px;color: red; background-color: #FFF;padding: 15px; text-align: center">
+			Maintenance Mode Is On
+		</h1>';
+}
+
+/**
+ * set session path to our local app directory in case of permissions issues
+ */
+// ini_set('session.save_path', '../runtime/session');
+
+/**
  * Getting global setting from the env file
  * enable error reporting if the env is dev
  */
 
+// check if the .env exist
+if (!file_exists(__DIR__ . "/../.env"))
+	die('.env file not found, please set it up first (use the .env.example template)');
+
 $config = parse_ini_file(__DIR__ . "/../.env");
- if (isset($config['env']) && $config['env'] === 'dev')
- 	error_reporting(E_ALL ^ E_DEPRECATED);
- else
- 	error_reporting(0);
+if (isset($config['env']) && $config['env'] === 'dev'){
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL ^ E_DEPRECATED);
+} else {
+	ini_set('display_errors', 0);
+	ini_set('display_startup_errors', 0);
+	error_reporting(0);
+}
 
 /**
  * require our autoloader
@@ -45,14 +83,25 @@ use core\Application;
  * creating a new instance of the Application class
  */
 
-$app = new Application(dirname(__DIR__));
+$app = new Application(dirname(__DIR__), 'api');
 
 /**
  * require the routes of our application
  */
 
-require_once "../routeApi.php";
+function shutdown($start)
+{
+	/**
+	 * todo: write to a file and retrieve it using api
+	 */
+}
 
+register_shutdown_function('shutdown', $start);
+
+/**
+ * change header to json
+ */
+header('Content-Type: application/json');
 
 /**
  * run our application
