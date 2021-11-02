14791cf21c4da2b9b304d03f5d13d83f90107d8c
m-agoumi <agoumihunter@gmail.com>
2021-11-03 00:38:12 +0100

    update dependencies injection

--- src/core/Router.php
+++ src/core/Router.php
@@ -14,6 +14,7 @@
 namespace core;
 
 use controller\Controller;
+use core\Db\DbModel;
 use core\Exception\ExpiredException;
 use core\Exception\NotFoundException;
 use ReflectionClass;
@@ -27,6 +28,7 @@ class Router
 	protected array $paths = [];
 	protected string $tmp;
 	private string $interface;
+	private $callback;
 
 	/**
 	 * Router constructor.
@@ -34,6 +36,7 @@ class Router
 	 * them
 	 * @param Request $request
 	 * @param Response $response
+	 * @param string $interface
 	 */
 	public function __construct(Request $request, Response $response, string $interface = 'web')
 	{
@@ -90,11 +93,32 @@ class Router
 		return self::registerRoute('post', $path, $callback);
 	}
 
+	/** register magic path (path with variable)
+	 * @param $path
+	 * @param $callback
+	 * @return Router
+	 */
+
 	public static function magic($path, $callback): Router
 	{
 		return self::registerRoute('magic', $path, $callback);
 	}
 
+	/** save route as a get and post request
+	 * @param string $path
+	 * @param $callback
+	 * @return Router|null
+	 */
+	public static function request(string $path, $callback): ?Router
+	{
+		self::registerRoute('post', $path, $callback);
+
+		return self::registerRoute('get', $path, $callback);
+	}
+
+	/** register a name for the current path
+	 * @param string $name
+	 */
 	public function name(string $name)
 	{
 		if (!isset($this->paths[$name]))
@@ -134,9 +158,10 @@ class Router
 	public function resolve()
 	{
 		$callback = $this->getCallbackOrFail();
+		$this->callback = $callback;
 
 		if (is_string($callback))
-			return $this->renderView($callback);
+			return render($callback);
 		
 		if (is_array($callback))
 			return $this->execArrayCallback($callback);
@@ -176,8 +201,6 @@ class Router
 
 	/**
 	 * @throws \ReflectionException
-	 * @throws NotFoundException
-	 * @throws ExpiredException
 	 */
 	protected function execArrayCallback($callback)
 	{
@@ -204,8 +227,6 @@ class Router
 	/**
 	 * @param $callback
 	 * @return array
-	 * @throws ExpiredException
-	 * @throws NotFoundException
 	 * @throws \ReflectionException
 	 */
 	protected function injectDependencies($callback): array
@@ -217,6 +238,7 @@ class Router
 		foreach ($reflector->getMethod($callback[1])->getParameters() as $param) {
 			$modelName = $param->name;
 			$modelType = $param->getClass()->name ?? NULL;
+
 			array_push($params, $this->injectClassOrModule($modelType, $modelName, $callback));
 		}
 
@@ -226,56 +248,98 @@ class Router
 	/**
 	 * @param $type
 	 * @param $name
-	 * @param $callback
 	 * @return mixed
-	 * @throws NotFoundException
-	 * @throws ExpiredException
 	 */
-	protected function injectClassOrModule($type, $name, $callback)
+	protected function injectClassOrModule($type, $name)
 	{
-		if (Application::isAppProperty($name) && $name != 'user') {
-			$param = Application::$APP->$name;
-		} else {
-			if (class_exists($type)) {
-				$param = new $type();
-				$relations = [];
-
-				if (method_exists($param, 'relationships'))
-					$relations = $param->relationships();
-
-				$primaryKey = $callback[2] ?? '';
-
-				if ($name == $primaryKey)
-					$primaryKey = $param->primaryKey();
-
-				if (isset($callback[3])) {
-					$param = $param::findOne([$primaryKey => $callback[3]], $relations);
-
-					if (!$param->getId())
-						if (strpos($callback[2], 'token') !== false)
-							throw new ExpiredException('Invalid token');
-						else
-							throw new NotFoundException();
-				}
-			} else {
-				if (isset($callback[3])) {
-					$param = $callback[3];
-				} else {
-					die('class ' . $type . ' not found while trying to inject it');
-				}
-			}
-		}
-
-		return $param;
+		if (Application::isAppProperty($name) && $name != 'user')
+			return Application::$APP->$name;
+		else
+			return $this->injectModule($type);
 	}
 
-	protected function renderView(string $callback)
-	{
-		return Application::$APP->view->renderView($callback);
-	}
-
+	/**
+	 * load routes from the corresponding interface to the running app
+	 */
 	private function getRoutes()
 	{
 		include Application::$ROOT_DIR . "/routes/" . $this->interface . ".php";
 	}
+
+	/** inject module
+	 * @param $type
+	 * @return object|void
+	 * @throws \ReflectionException
+	 */
+	private function injectModule($type)
+	{
+		if (class_exists($type))
+			return $this->createModuleInstance($type);
+
+		die('class ' . $type . ' not found while trying to inject it');
+	}
+
+	/**
+	 * @throws \ReflectionException
+	 * @throws ExpiredException
+	 * @throws NotFoundException
+	 */
+	private function createModuleInstance(string $type): object
+	{
+		/** @var  $arguments array of constructor arguments */
+		$arguments = $this->getClassConstructorArguments($type);
+		$reflector = new ReflectionClass($type);
+		$name = $reflector->getShortName();
+
+		$instance = $reflector->newInstanceArgs($arguments);
+
+		$relations = [];
+
+		if (method_exists($instance, 'relationships'))
+			$relations = $instance->relationships();
+
+		$primaryKey = $this->callback[2] ?? '';
+
+		if ($name == $primaryKey)
+			$primaryKey = $instance->primaryKey();
+
+		if ($instance instanceof DbModel) {
+			if (isset($this->callback[3])) {
+				$instance = $instance::findOne([$primaryKey => $this->callback[3]], $relations);
+
+				if (!$instance->getId())
+					if (strpos($this->callback[2], 'token') !== false)
+						throw new ExpiredException('Invalid token');
+					else
+						throw new NotFoundException();
+			}
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * @throws \ReflectionException
+	 */
+	private function getClassConstructorArguments(string $type): ?array
+	{
+		$rf = new ReflectionClass($type);
+
+		$constructorRef = $rf->getConstructor();
+		$constructorArguments = $constructorRef ? $constructorRef->getParameters() : [];
+		if (!count($constructorArguments))
+			return [];
+
+		$arguments = [];
+
+		foreach ($constructorArguments as $argument)
+		{
+			$type = $argument->getType();
+			$name = $argument->getName();
+			$arguments[] = $this->injectClassOrModule($type, $name);
+		}
+
+		return $arguments;
+	}
 }
+
