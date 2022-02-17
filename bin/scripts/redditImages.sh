6c74982969c1b11e14bd16a88834c508d5fec90d
yassineelidrissi <yassineelidrissi02@gmail.com>
2022-02-17 19:40:55 +0100

    Modify the content of the scrapped link

--- bin/scripts/redditImages.sh
+++ bin/scripts/redditImages.sh
@@ -1,4 +1,4 @@
-curl -s -A "reddit scraper" "https://www.reddit.com/r/pics.json?limit=100" | \
+curl -s -A "reddit scraper" "https://www.reddit.com/r/ProgrammerHumor.json?limit=100" | \
 jq -r '.data.children | map(.data.url_overridden_by_dest)[] | select(.)' | \
 grep -E "(jpg|jpeg|png|mp4)$" | \
 xargs wget -U "reddit downloader"
